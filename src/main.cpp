c59106bd539f291a1aab978fa088a3b44e4c604e
Mattias Lasersköld <mattias.l.sk@gmail.com>
2022-10-18 16:10:40 +0200

    use file_sink

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 //#include <boost/iostreams/filter/zlib.hpp>
 #include <boost/iostreams/copy.hpp>
+#include <boost/iostreams/device/file.hpp>
 #include <boost/iostreams/filter/gzip.hpp>
 #include <boost/iostreams/filtering_stream.hpp>
 #include <fstream>
@@ -9,27 +10,24 @@
 int main(int argc, char *argv[]) {
     using std::literals::operator""s;
     {
-        auto file = std::ofstream{"test.gz"};
         auto out = boost::iostreams::filtering_ostream{};
+        out.push(boost::iostreams::gzip_compressor());
+        out.push(boost::iostreams::file_sink{"test.gz"});
 
-        out.push(file);
-        out.push(boost::iostreams::gzip_compressor{});
-
-        auto str = "hello hello there\n"s;
+        auto str = "hello hello there!\n"s;
 
         out << str;
     }
 
     {
-        auto file = std::ifstream{"test.gz"};
 
-        if (!file) {
+        auto in = boost::iostreams::filtering_istream{};
+        in.push(boost::iostreams::gzip_decompressor{});
+        in.push(boost::iostreams::file_source{"test.gz"});
+        if (!in) {
             std::cout << "could not open file\n";
             std::terminate();
         }
-        auto in = boost::iostreams::filtering_istream{};
-        in.push(boost::iostreams::gzip_decompressor{});
-        in.push(file);
 
         boost::iostreams::copy(in, std::cout);
         std::cout.flush();
