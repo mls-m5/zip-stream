eaee245fe394a9d5fb215dd05c3179b1c0bffbc3
Mattias Lasersköld <mattias.l.sk@gmail.com>
2022-10-18 16:14:00 +0200

    Extract into functions

--- src/main.cpp
+++ src/main.cpp
@@ -5,31 +5,38 @@
 #include <boost/iostreams/filtering_stream.hpp>
 #include <fstream>
 #include <iostream>
-#include <sstream>
+#include <memory>
+
+std::shared_ptr<std::ostream> createSink() {
+    auto out = std::make_unique<boost::iostreams::filtering_ostream>();
+    out->push(boost::iostreams::gzip_compressor());
+    out->push(boost::iostreams::file_sink{"test.gz"});
+    return out;
+}
+
+std::shared_ptr<std::istream> createSource() {
+
+    auto in = std::make_unique<boost::iostreams::filtering_istream>();
+    in->push(boost::iostreams::gzip_decompressor{});
+    in->push(boost::iostreams::file_source{"test.gz"});
+    if (!in) {
+        std::cout << "could not open file\n";
+        std::terminate();
+    }
+    return in;
+}
 
 int main(int argc, char *argv[]) {
     using std::literals::operator""s;
     {
-        auto out = boost::iostreams::filtering_ostream{};
-        out.push(boost::iostreams::gzip_compressor());
-        out.push(boost::iostreams::file_sink{"test.gz"});
-
+        auto out = createSink();
         auto str = "hello hello there!\n"s;
-
-        out << str;
+        *out << str;
     }
 
     {
-
-        auto in = boost::iostreams::filtering_istream{};
-        in.push(boost::iostreams::gzip_decompressor{});
-        in.push(boost::iostreams::file_source{"test.gz"});
-        if (!in) {
-            std::cout << "could not open file\n";
-            std::terminate();
-        }
-
-        boost::iostreams::copy(in, std::cout);
+        auto in = createSource();
+        boost::iostreams::copy(*in, std::cout);
         std::cout.flush();
     }
 
